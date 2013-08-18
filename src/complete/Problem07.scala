524c1e1931bd2a81ace0a0607f4f8cda3f36f31b
Paul Kinsky <pkinsky@gmail.com>
2013-08-18 12:09:06 -0400

    refactoring

--- src/complete/Problem07.scala
+++ src/complete/Problem07.scala
@@ -1,23 +1,24 @@
 package complete
 
-
 object Problem7 {
+	//What is the 10 001st prime number?
+	def main(args: Array[String]): Unit = {
+		assert(primes_until(10001).last == 104743)
+	}
 
-  def main(args: Array[String]): Unit = {
-    println(primes_until(10001).last)
-  }
-  
-  def primes_until(until: Int): Seq[Int] = {
-    @scala.annotation.tailrec
-    def loop(i: Int, primes: Seq[Int]): Seq[Int] = {
-      if (primes.length >= until) primes // we reached the desired end
-      else {
-        if (primes exists (x => i % x == 0)) loop(i + 2, primes) // we already found a factor of this i
-        else loop(i + 2, primes :+ i)
-      }
-    }
-    if (until <= 1) Seq(2)
-    else loop(3, Seq(2))
-  }
+	def primes_until(until: Int): Seq[Int] = {
+		@scala.annotation.tailrec
+		def loop(i: Int, primes_found: Seq[Int]): Seq[Int] = {
+			if (primes_found.length >= until) 
+				primes_found
+			else {
+				if (primes_found exists (x => i % x == 0)) 
+					loop(i + 2, primes_found) // a prime exists that evenly divides this, continue
+				else loop(i + 2, primes_found :+ i)	// a new prime is found
+			}
+		}
+		if (until <= 1) Seq(2)
+		else loop(3, Seq(2))
+	}
 
 }
